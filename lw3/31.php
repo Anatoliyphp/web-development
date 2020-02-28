7e24e5961233ba75318bc89ab3cb04567781774b
Anatoliyphp <tolian1471@gmail.com>
2020-02-28 21:26:13 +0300

    bibb

---
+++ lw3/31.php
@@ -0,0 +1,11 @@
+<?php
+  function getGetParametr(string $text): ?string
+  {
+    return isset($_GET[$text]) ? (string)$_GER[$text] : null;
+  }
+
+  $text = getGetParametr('text');
+  $text = trim($text);
+  $text = preg_replace('/\s+/', ' ', $text);
+  header("Content-Type: text/plain");
+  echo $text;
