5ca20f1ec6849bdfd0a62dcb4f6197ffba711aa2
Anatoliyphp <tolian1471@gmail.com>
2020-04-21 16:23:38 +0300

    jit

--- lw6/form.php
+++ lw6/form.php
@@ -1,10 +1,11 @@
 <?php
       $txt = ".txt";
-      if (($_POST['mail']) and ($_POST['name']) and ($_POST['country']))
+      if (($_POST['mail']) and ($_POST['name']) and ($_POST['country']) and ($_POST['text']))
       {
       $hello = fopen(htmlspecialchars($_POST['mail']).$txt, "w+");
       fwrite($hello, htmlspecialchars($_POST['name']."\n"));
-      fwrite($hello, htmlspecialchars($_POST['country']));
+      fwrite($hello, htmlspecialchars($_POST['country']."\n"));
+      fwrite($hello, htmlspecialchars($_POST['text']."\n"));
       fclose($hello);
       echo 'Succesful';
       }
