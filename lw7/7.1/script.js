4947b7760ac4f45accebcfd382addfab75523bfe
Anatoliyphp <tolian1471@gmail.com>
2020-05-16 18:21:34 +0300

    pit

--- lw7/7.1/script.js
+++ lw7/7.1/script.js
@@ -3,6 +3,19 @@ isPrimeNumber(n);
 
 function isPrimeNumber(n){
   let isNumberArray = n.every(isInteger);
+  let isPrime = false;
+
+  function checkPrime(n){
+    for(let i = 2; i <= n; i++){
+      isPrime = true;
+        for(let j = 2; j < i; j++){
+          if ((i % j) === 0){
+            isPrime = false;
+            break;
+          }
+        }
+    }
+  }
 
   function isInteger(elem){
   	return((typeof elem) === "number");
@@ -14,14 +27,12 @@ function isPrimeNumber(n){
     }
     else{
       for (let y = 0; y < n.length; y++){
-        for(let i = 2; i <= n[y]; i++){
-    	  console.log(n[y], 'is prime number');
-    	  for(let j = 2; j < i; j++){
-    	  	if ((i % j) === 0){
-              console.log(n[y], 'is not prime number');
-              break;
-            }
-          }
+        checkPrime(n[y]);
+        if (isPrime){
+          console.log(n[y], 'is prime number');
+        }
+        else{
+          console.log(n[y], 'is not prime number');
         }
       }
     }
