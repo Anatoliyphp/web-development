248c3a2596a3e0e9c2d1ec2b5f6930f454cb9e9f
Anatoliyphp <tolian1471@gmail.com>
2020-05-16 18:20:48 +0300

    lit

--- lw3/34.php
+++ lw3/34.php
@@ -2,18 +2,24 @@
   function getGetParameter(string $text):?string
     {
       return isset($_GET[$text]) ? (string)$_GET[$text] : null;
-    };
+    }
 
-  $name = getGETparameter('first_name');
-  $second = getGETparameter('last_name');
+  $firstname = getGETparameter('first_name');
+  $lastname = getGETparameter('last_name');
   $age = getGETparameter('age');
   $mail = getGETparameter('email');
-  if ($mail != null){
+  if (file_exists('data')){
+  	echo('Saved ');
+  }
+  else{
+    mkdir('data', 0777, true);
+  }
+  if ($mail){
   	$dir = 'data/'.$mail.'.txt';
   	$survay = fopen($dir, 'w+');
-  	fwrite($survay, $name."\n");
-    fwrite($survay, $second."\n");
-    fwrite($survay, $age."\n");
+  	fwrite($survay, $firstname.PHP_EOL);
+    fwrite($survay, $lastname.PHP_EOL);
+    fwrite($survay, $age.PHP_EOL);
     fclose($survay);
   	echo('Successful');
   }
