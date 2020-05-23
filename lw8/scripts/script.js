70bdd23efa529b0e7d261361a68ff95d2ab805cc
Anatoliyphp <tolian1471@gmail.com>
2020-05-23 17:32:47 +0300

    pit

--- lw8/scripts/script.js
+++ lw8/scripts/script.js
@@ -4,35 +4,34 @@ const slideBox = document.getElementById('block');
 let I = 1;
 let J = 12;
 
-right.onclick = changerightPosition;
-function changerightPosition() {
-	if (J != 12){
-		I = J;
-		slideBox.appendChild(document.getElementById(I+1));
-		J++;
-	}
-	else{
-	    if (I > 12){
-		  I = 1;
-	    }
-	    slideBox.appendChild(document.getElementById(I));
-	}
-    I++;
+right.onclick = changeRightPosition;
+function changeRightPosition() {
+  if (J != 12){
+    I = J + 1;
+    slideBox.appendChild(document.getElementById(I));
+    J++;
+  }
+  else{
+    if (I > 12 || I < 1){
+      I = 1;
+    }
+    slideBox.appendChild(document.getElementById(I));
+  }
+  I++;
 }
 
-left.onclick = changeleftPosition;
-function changeleftPosition() {
-	if (I != 1){
-		J = I;
-		slideBox.prepend(document.getElementById(J));
-		I--;
-	}
-	else{
-		if (J < 1){
-		  J = 12;
-	    }
-	    slideBox.prepend(document.getElementById(J));
-	}
-	J--;
+left.onclick = changeLeftPosition;
+function changeLeftPosition() {
+  if (I != 1){
+    J = I - 1;
+    slideBox.prepend(document.getElementById(J));
+    I--;
+  }
+  else{
+    if (J < 1 || J > 12){
+      J = 12;
+    }
+    slideBox.prepend(document.getElementById(J));
+  }
+  J--;
 }
-
