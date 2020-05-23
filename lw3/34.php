4afdaa5c756a50c6fc255430a0ab285d7a573c7b
Anatoliyphp <tolian1471@gmail.com>
2020-05-23 17:25:44 +0300

    oit

--- lw3/34.php
+++ lw3/34.php
@@ -1,29 +1,25 @@
 <?php
-  function getGetParameter(string $text):?string
+    function getGetParameter(string $text):?string
     {
-      return isset($_GET[$text]) ? (string)$_GET[$text] : null;
+        return isset($_GET[$text]) ? (string)$_GET[$text] : null;
     }
-
-  $firstname = getGETparameter('first_name');
-  $lastname = getGETparameter('last_name');
-  $age = getGETparameter('age');
-  $mail = getGETparameter('email');
-  if (file_exists('data')){
-  	echo('Saved ');
-  }
-  else{
-    mkdir('data', 0777, true);
-  }
-  if ($mail){
-  	$dir = 'data/'.$mail.'.txt';
-  	$survay = fopen($dir, 'w+');
-  	fwrite($survay, $firstname.PHP_EOL);
-    fwrite($survay, $lastname.PHP_EOL);
-    fwrite($survay, $age.PHP_EOL);
-    fclose($survay);
-  	echo('Successful');
-  }
-  else
-  {
-  	echo('error');
-  }
+    define("firstName", getGETparameter('first_name'));
+    define("lastName", getGETparameter('last_name'));
+    define("age", getGETparameter('age'));
+    define("mail", getGETparameter('email'));
+    if (!file_exists('data')){
+        mkdir('data', 0777, true);
+    }
+    if (mail){
+        $dir = 'data/' . mail . '.txt';
+        $survay = fopen($dir, 'w+');
+        fwrite($survay, firstName . PHP_EOL);
+        fwrite($survay, lastName . PHP_EOL);
+        fwrite($survay, age . PHP_EOL);
+        fclose($survay);
+        echo('Successful');
+    }
+    else
+    {
+        echo('error');
+    }
